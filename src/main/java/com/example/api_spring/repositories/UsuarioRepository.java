9e990292dc5160f45aa21f6b24f59866c0879cbb
Heitorkatsuki <heitor.katsuki@germinare.org.br>
2024-09-24 16:46:51 -0300

    fix/ ajustando o application.properties

--- src/main/java/com/example/api_spring/repositories/UsuarioRepository.java
+++ src/main/java/com/example/api_spring/repositories/UsuarioRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Usuario findByEmail(String email);
+    Usuario findByNome(String nome);
+
+//    @Procedure
+//    Usuario inserir_usuario_e_interesse(Usuario usuario, UsuarioInteresse usuarioInteresse);
 }
