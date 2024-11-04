8d888defea3c549022179e262bdc875c1a1b8d4e
MariaMsschs <maria.msschs@gmail.com>
2024-11-04 17:50:53 -0300

    checar se vendedor existe

--- src/main/java/com/example/api_spring/api/repositories/VendedorRepository.java
+++ src/main/java/com/example/api_spring/api/repositories/VendedorRepository.java
@@ -1,9 +1,8 @@
 package com.example.api_spring.api.repositories;
 
-import com.example.api_spring.api.models.Anuncio;
 import com.example.api_spring.api.models.Vendedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendedorRepository extends JpaRepository<Vendedor,Long> {
-    Vendedor findVendedorByIdUsuario(Long id);
+    boolean existsById(Long id);
 }
