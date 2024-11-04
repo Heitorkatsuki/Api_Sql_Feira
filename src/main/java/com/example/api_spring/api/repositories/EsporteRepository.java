664b0938ac20a4f742936cf263d6590fa520acf1
MariaMsschs <maria.msschs@gmail.com>
2024-11-04 08:56:44 -0300

    nova feature

--- src/main/java/com/example/api_spring/api/repositories/EsporteRepository.java
+++ src/main/java/com/example/api_spring/api/repositories/EsporteRepository.java
@@ -1,7 +1,9 @@
 package com.example.api_spring.api.repositories;
 
+import com.example.api_spring.api.models.Anuncio;
 import com.example.api_spring.api.models.Esporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EsporteRepository extends JpaRepository<Esporte,Long> {
+    Esporte findEsporteByIdEsporte(Long id);
 }
--- src/main/java/com/example/api_spring/api/services/EsporteService.java
+++ src/main/java/com/example/api_spring/api/services/EsporteService.java
@@ -1,10 +1,12 @@
 package com.example.api_spring.api.services;
 
+import com.example.api_spring.api.models.Anuncio;
 import com.example.api_spring.api.models.ApiResponseAthleta;
 import com.example.api_spring.api.models.Esporte;
 import com.example.api_spring.api.repositories.EsporteRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,6 +30,20 @@ public class EsporteService {
             return new ApiResponseAthleta(false, "Não há esportes no banco", null, "Vazio");
         } catch (Exception e){
             return new ApiResponseAthleta(false, "Falha ao retornar esportes", null, null);
+        }
+    }
+
+    public ApiResponseAthleta listarEsportePorId(Long id){
+        try {
+            Esporte response = esporteRepository.findEsporteByIdEsporte(id);
+            if(response != null){
+                List<Object> esporteList = new ArrayList<>();
+                esporteList.add(response);
+                return new ApiResponseAthleta(true, "Esporte pego com sucesso", esporteList, null);
+            }
+            return new ApiResponseAthleta(false, "Esporte não existe no banco", null, "Vazio");
+        } catch (Exception e){
+            return new ApiResponseAthleta(false, "Não foi possível pegar o esporte", null, null);
         }
     }
 }
